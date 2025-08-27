b7b63efa543c65988ca6f73db339519f8c8a7c43
yassminn8954 <ens-yasminscotti@ugv.edu.br>
2025-08-27 00:27:50 -0300

    routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,24 +4,18 @@ const router = express.Router();
 
 const carro = require("./carro.json");
 
-// Rota inicial -> exibe login.html (Incoming Change)
+// Rota inicial -> exibe login.html 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
-// Rota inicio -> exibe inicio.html (Incoming Change)
+// Rota inicio -> exibe inicio.html 
 router.get("/inicio", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "inicio.html"));
 });
 
-// Exemplo adicional -> API retorna JSON de carro (seu HEAD)
-router.get("/carro", (req, res) => {
-  res.json(carro);
-});
-
-// Middleware 404 -> serve página personalizada (seu HEAD)
 router.use((req, res) => {
-  res.status(404).sendFile("./view/404.html", { root: __dirname });
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 module.exports = router;
